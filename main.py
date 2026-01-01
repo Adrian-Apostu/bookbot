255f8e75847910cb6e1c3be6838393c01a021aed
Adrian <apostuadrian6@gmail.com>
2026-01-01 18:17:50 +0200

    updated the way main.py prints the results

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, sort_characters_by_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -7,5 +7,14 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     words = count_words(book_text)
-    print(f"Found {words} total words)")
+    characters = count_characters(book_text)
+    character_counts = sort_characters_by_count(characters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for entry in character_counts:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
 main()
