67c5223add39bab087f51e5d425071ee23578eca
Adrian <apostuadrian6@gmail.com>
2026-01-01 18:46:45 +0200

    enhance main.py and stats.py to include character counting and sorting functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_characters_by_count
 
 def get_book_text(path_to_file):
@@ -5,12 +6,15 @@ def get_book_text(path_to_file):
         return file.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     words = count_words(book_text)
     characters = count_characters(book_text)
     character_counts = sort_characters_by_count(characters)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
