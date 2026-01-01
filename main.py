15e44e44013df1848445a147945643b2a5290d5a
Adrian <apostuadrian6@gmail.com>
2026-01-01 18:12:15 +0200

    Revert "enhance main.py and stats.py to include character counting and sorting functionality"
    This reverts commit a2d53f6cf7149340dcb09a43421b3e28cacf3296.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters, sort_characters_by_count
+from stats import count_words
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -7,17 +7,5 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     words = count_words(book_text)
-    characters = count_characters(book_text)
-    charachter_counts = sort_characters_by_count(characters)
-    result = """
-============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count ----------
-Found 75767 total words
---------- Character Count -------""" + "\n"
-    for entry in charachter_counts:
-        result += f"{entry['char']}: {entry['num']}\n"
-    result += """
-============= END ==============="""
-    print(result)
+    print(f"Found {words} total words)")
 main()
