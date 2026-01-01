a2d53f6cf7149340dcb09a43421b3e28cacf3296
Adrian <apostuadrian6@gmail.com>
2026-01-01 18:04:11 +0200

    enhance main.py and stats.py to include character counting and sorting functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, sort_characters_by_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -7,5 +7,17 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     words = count_words(book_text)
-    print(f"Found {words} total words)")
+    characters = count_characters(book_text)
+    charachter_counts = sort_characters_by_count(characters)
+    result = """
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found 75767 total words
+--------- Character Count -------""" + "\n"
+    for entry in charachter_counts:
+        result += f"{entry['char']}: {entry['num']}\n"
+    result += """
+============= END ==============="""
+    print(result)
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,28 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    lowercased = text.lower()
+    count_characters = {}
+    for char in lowercased:
+        if char in count_characters:
+            count_characters[char] += 1
+        else:
+            count_characters[char] = 1
+    return count_characters
+    
+def sort_on(items):
+    return items["num"]
+    
+def sort_characters_by_count(character_counts):
+    counts_list = []
+    entry = {}
+    for char in character_counts:
+        if char.isalpha() is False:
+            continue
+        entry["char"] = char
+        entry["num"] = character_counts[char]
+        counts_list.append(entry)
+        entry = {}
+    counts_list.sort(key=sort_on, reverse=True)
+    return counts_list
