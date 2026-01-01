199c57920accccccdc020807760ab07e6fbed966
Adrian <apostuadrian6@gmail.com>
2026-01-01 15:45:34 +0200

    refactor main.py to count and display total words in the book text

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from stats import count_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-
+    words = count_words(book_text)
+    print(f"Found {words} total words)")
 main()
